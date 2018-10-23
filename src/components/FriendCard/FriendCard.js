29d6f22527c24c034c2d2fa2c9ccae4a42891654
mmcadet@gmail.com <34467088+mmcadet@users.noreply.github.com>
2018-10-23 17:43:27 -0500

    fixing onClick

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -1,26 +1,47 @@
 import React from "react";
 import "./FriendCard.css";
 
-const FriendCard = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
+
+class FriendCard extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      id: props.id,
+      name: props.name,
+      image: props.image,
+      occupation: props.occupation,
+      location: props.location
+
+    }
+  }
+
+  render(){
+    return(
+
+      <div   className="card">
+      <div className="img-container">
+        <img onClick={()=>this.props.onClick(this.state.id)}  value={this.props.id} alt={this.props.name} src={this.props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {this.props.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong> {this.props.occupation}
+          </li>
+          <li>
+            <strong>Address:</strong> {this.props.location}
+          </li>
+        </ul>
+      </div>
+      <span className="remove">𝘅</span>
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Address:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
-    <span className="remove">𝘅</span>
-  </div>
-);
+
+    )
+  }
+}
+
+
 
 export default FriendCard;
--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,6 +1,48 @@
 import React from "react";
 import "./Wrapper.css";
+import FriendCard from "../FriendCard/FriendCard";
+import friends from "../../friends.json";
 
-const Wrapper = props => <div className="wrapper">{props.children}</div>;
+class Wrapper extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selected : []
+        }
+       
+    }
+    handleClick=(event) => {
+        console.log(event);
+        this.setState({
+            selected: [...this.state.selected, event]
+        })
+        console.log(this.state.selected);
+        // fill selected with ids then get them to display
+    }
+
+    render(){
+        
+        return(
+            <div className="wrapper">
+                 {
+                   
+                 friends.map(friend => (
+                <FriendCard
+                  removeFriend={this.removeFriend}
+                  id={friend.id}
+                  onClick={this.handleClick }
+                  key={friend.id}
+                  name={friend.name}
+                  image={friend.image}
+                  occupation={friend.occupation}
+                  location={friend.location}
+                />
+              ))}
+
+            </div>
+           
+        )
+    }
+}
 
 export default Wrapper;
