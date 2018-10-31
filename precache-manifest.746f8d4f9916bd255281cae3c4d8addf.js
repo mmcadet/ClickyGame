0e9c73b8353575bd75ef49b7b88066380525dfa1
mmcadet@gmail.com <34467088+mmcadet@users.noreply.github.com>
2018-10-30 23:33:55 -0500

    Updates

---
+++ precache-manifest.746f8d4f9916bd255281cae3c4d8addf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "af27ee5121eba3a8d475",
+    "url": "/ClickyGame/static/js/main.af27ee51.chunk.js"
+  },
+  {
+    "revision": "3db5b7aa35c1da3179d6",
+    "url": "/ClickyGame/static/js/1.3db5b7aa.chunk.js"
+  },
+  {
+    "revision": "af27ee5121eba3a8d475",
+    "url": "/ClickyGame/static/css/main.d27675c4.chunk.css"
+  },
+  {
+    "revision": "492240dfaf426fba8e44421ea2bc1737",
+    "url": "/ClickyGame/index.html"
+  }
+];
